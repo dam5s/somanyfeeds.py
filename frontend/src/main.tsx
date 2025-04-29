eeb48d9e0f9ce03b33f1d2a458f555f337522ce2
Damien Le Berrigaud <git@itsdamo.com>
2025-04-29 11:45:27 -0600

    Simplify default api url

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,6 @@ import {App} from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App apiUrl={'http://localhost:8081'}/>
+        <App apiUrl={''}/>
     </StrictMode>,
 );
