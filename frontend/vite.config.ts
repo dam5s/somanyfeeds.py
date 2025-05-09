d784fa55556ee4ce66216806625aea058fd8b4a9
Damien Le Berrigaud <git@itsdamo.com>
2025-05-09 14:14:07 -0600

    Reformat vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
-import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: process.env.VITE_BASE_PATH,
     plugins: [react()],
     server: {
         port: 8080,
-        proxy: {'/api': 'http://localhost:8081'},
+        proxy: { "/api": "http://localhost:8081" },
     },
-});
+})
