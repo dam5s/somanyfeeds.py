c0d538411df11161eec3f3ad865e37a3fbccc783
Damien Le Berrigaud <git@itsdamo.com>
2025-05-13 12:18:02 -0600

    Use uuid6 library instead of uuid7

--- backend/pkgs/database_support/uuid_v7.py
+++ backend/pkgs/database_support/uuid_v7.py
@@ -1,8 +1,7 @@
-from typing import cast
 from uuid import UUID
 
-from uuid_extensions import uuid7
+import uuid6
 
 
 def generate_uuid_v7() -> UUID:
-    return cast(UUID, uuid7())
+    return uuid6.uuid7()
